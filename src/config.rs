1bd874ea514ddf509617aba05fedb2f560ef26d6
Jared Beller <jbellerb@vt.edu>
2023-02-20 19:34:09 -0500

    basic config and command line parsing

---
+++ src/config.rs
@@ -0,0 +1,41 @@
+use std::net::{IpAddr, Ipv6Addr};
+use std::path::PathBuf;
+
+use serde::Deserialize;
+
+const DEFAULT_ADDR: IpAddr = IpAddr::V6(Ipv6Addr::LOCALHOST);
+const DEFAULT_PORT: u16 = 8080;
+
+const DEFAULT_AUTH_URL: &str = "https://accounts.google.com/o/oauth2/v2/auth";
+
+#[derive(Deserialize, Clone, Debug)]
+pub struct Config {
+    #[serde(default = "default_addr")]
+    pub ip: IpAddr,
+    #[serde(default = "default_port")]
+    pub port: u16,
+    pub google: GoogleConfig,
+}
+
+#[derive(Deserialize, Clone, Debug)]
+pub struct GoogleConfig {
+    pub client_id: String,
+    pub client_secret: String,
+    pub allowed_domains: Vec<String>,
+    #[serde(default = "default_auth_url")]
+    pub auth_url: String,
+    pub service_account_file: PathBuf,
+    pub admin_email: String,
+}
+
+const fn default_addr() -> IpAddr {
+    DEFAULT_ADDR
+}
+
+const fn default_port() -> u16 {
+    DEFAULT_PORT
+}
+
+fn default_auth_url() -> String {
+    DEFAULT_AUTH_URL.to_string()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,49 @@
-fn main() {
-    println!("Hello, world!");
+mod config;
+
+use std::env::var;
+use std::fs::read_to_string;
+use std::path::PathBuf;
+
+use config::Config;
+
+use anyhow::{Context, Result};
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author, version)]
+/// poser is a simple, opinionated authentication provider for nginx
+///
+/// poser authenticates with Google using OpenID Connect and then uses the
+/// Google Workspace Admin SDK to determine what groups a user is a part of.
+/// Basic information about the users as well as what groups they are a part
+/// of is returned to nginx in a PASETO v4 token, which is then passed to the
+/// application.
+struct Args {
+    /// Configuration file
+    #[arg(long, default_value = "/data/config/config.toml")]
+    config: PathBuf,
+}
+
+fn main() -> Result<()> {
+    init_logging();
+
+    let args = Args::parse();
+
+    let config: Config = toml::from_str(&read_to_string(&args.config).with_context(|| {
+        format!(
+            "Failed to read config file at {}",
+            args.config.to_string_lossy()
+        )
+    })?)
+    .context("Failed to parse config file")?;
+
+    println!("{:#?}", config);
+
+    Ok(())
+}
+
+fn init_logging() {
+    tracing_subscriber::fmt()
+        .with_env_filter(var("RUST_LOG").unwrap_or("info".to_string()))
+        .init();
 }
