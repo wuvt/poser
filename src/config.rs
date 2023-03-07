945ea27231eb34b16a1275630eb9b67bda5bc96e
Jared Beller <jbellerb@vt.edu>
2023-03-07 00:56:10 -0500

    switch to environment variables for config

--- src/config.rs
+++ src/config.rs
@@ -1,69 +1,161 @@
-use std::net::{IpAddr, Ipv6Addr};
+use std::env::{var, VarError};
+use std::net::SocketAddr;
 use std::path::PathBuf;
 
 use base64ct::{Base64, Encoding};
 use pasetors::{keys::SymmetricKey, version4::V4};
-use serde::{Deserialize, Deserializer};
+use thiserror::Error;
+use tracing::error;
 
-const DEFAULT_ADDR: IpAddr = IpAddr::V6(Ipv6Addr::LOCALHOST);
-const DEFAULT_PORT: u16 = 8080;
+#[derive(Error, Clone, Debug)]
+pub enum ConfigError {
+    #[error("invalid environment variable")]
+    InvalidEnvVar,
+    #[error("invalid socket address")]
+    InvalidAddr,
+    #[error("invalid base64")]
+    InvalidBase64,
+    #[error("invalid symmetric key")]
+    InvalidSymmetricKey,
+    #[error("missing cookie secret")]
+    MissingCookieSecret,
+    #[error("missing Google client id")]
+    MissingClientId,
+    #[error("missing Google client secret")]
+    MissingClientSecret,
+    #[error("missing Google admin email")]
+    MissingAdminEmail,
+}
+
+// Environment variables for each config option
+const ENV_LISTEN_ADDR: &str = "POSER_AUTH_LISTEN_ADDR";
+
+const ENV_COOKIE_NAME: &str = "POSER_AUTH_COOKIE_NAME";
+const ENV_COOKIE_SECRET: &str = "POSER_AUTH_COOKIE_SECRET";
+
+const ENV_GOOGLE_CLIENT_ID: &str = "POSER_AUTH_GOOGLE_CLIENT_ID";
+const ENV_GOOGLE_CLIENT_SECRET: &str = "POSER_AUTH_GOOGLE_CLIENT_SECRET";
+const ENV_GOOGLE_ALLOWED_DOMAINS: &str = "POSER_AUTH_GOOGLE_ALLOWED_DOMAINS";
+const ENV_GOOGLE_AUTH_URL: &str = "POSER_AUTH_GOOGLE_AUTH_URL";
+const ENV_GOOGLE_SERVICE_ACCOUNT: &str = "POSER_AUTH_GOOGLE_SERVICE_ACCOUNT";
+const ENV_GOOGLE_ADMIN_EMAIL: &str = "POSER_AUTH_GOOGLE_ADMIN_EMAIL";
+
+// Default values for some config options
+const DEFAULT_LISTEN_ADDR: &str = "127.0.0.1:8080";
 
 const DEFAULT_COOKIE_NAME: &str = "_poser_auth";
 
-const DEFAULT_AUTH_URL: &str = "https://accounts.google.com/o/oauth2/v2/auth";
+const DEFAULT_GOOGLE_AUTH_URL: &str = "https://accounts.google.com/o/oauth2/v2/auth";
+const DEFAULT_GOOGLE_SERVICE_ACCOUNT: &str = "/data/service_account.json";
 
-#[derive(Deserialize, Clone, Debug)]
+#[derive(Clone, Debug)]
 pub struct Config {
-    #[serde(default = "default_addr")]
-    pub ip: IpAddr,
-    #[serde(default = "default_port")]
-    pub port: u16,
+    pub addr: SocketAddr,
     pub cookie: CookieConfig,
     pub google: GoogleConfig,
 }
 
-#[derive(Deserialize, Clone, Debug)]
+#[derive(Clone, Debug)]
 pub struct CookieConfig {
-    #[serde(default = "default_cookie_name")]
     pub name: String,
-    #[serde(deserialize_with = "from_base64")]
     pub secret: SymmetricKey<V4>,
 }
 
-#[derive(Deserialize, Clone, Debug)]
+#[derive(Clone, Debug)]
 pub struct GoogleConfig {
     pub client_id: String,
     pub client_secret: String,
     pub allowed_domains: Vec<String>,
-    #[serde(default = "default_auth_url")]
     pub auth_url: String,
     pub service_account_file: PathBuf,
     pub admin_email: String,
 }
 
-fn from_base64<'de, D>(deserializer: D) -> Result<SymmetricKey<V4>, D::Error>
-where
-    D: Deserializer<'de>,
-{
-    use serde::de::Error;
-
-    String::deserialize(deserializer)
-        .and_then(|s| Base64::decode_vec(&s).map_err(|e| Error::custom(e.to_string())))
-        .and_then(|b| SymmetricKey::<V4>::from(&b).map_err(|e| Error::custom(e.to_string())))
+impl Config {
+    pub fn try_env() -> Result<Self, ConfigError> {
+        parse_config()
+    }
 }
 
-const fn default_addr() -> IpAddr {
-    DEFAULT_ADDR
+pub fn parse_config() -> Result<Config, ConfigError> {
+    let addr = get_env(ENV_LISTEN_ADDR)?
+        .unwrap_or_else(|| DEFAULT_LISTEN_ADDR.to_string())
+        .parse()
+        .map_err(|_| {
+            error!("could not parse socket address");
+            ConfigError::InvalidAddr
+        })?;
+
+    let cookie = {
+        let name = get_env(ENV_COOKIE_NAME)?.unwrap_or_else(|| DEFAULT_COOKIE_NAME.to_string());
+
+        let secret_encoded = get_env(ENV_COOKIE_SECRET)?.ok_or_else(|| {
+            error!("expected cookie secret");
+            ConfigError::MissingCookieSecret
+        })?;
+        let secret_decoded = Base64::decode_vec(&secret_encoded).map_err(|_| {
+            error!("failed to decode cookie secret as base64");
+            ConfigError::InvalidBase64
+        })?;
+        let secret = SymmetricKey::<V4>::from(&secret_decoded).map_err(|_| {
+            error!("failed to interprete cookie secret as encryption key");
+            ConfigError::InvalidSymmetricKey
+        })?;
+
+        CookieConfig { name, secret }
+    };
+
+    let google = {
+        let client_id = get_env(ENV_GOOGLE_CLIENT_ID)?.ok_or_else(|| {
+            error!("expected Google client id");
+            ConfigError::MissingClientId
+        })?;
+
+        let client_secret = get_env(ENV_GOOGLE_CLIENT_SECRET)?.ok_or_else(|| {
+            error!("expected Google client secret");
+            ConfigError::MissingClientSecret
+        })?;
+
+        let allowed_domains = get_env(ENV_GOOGLE_ALLOWED_DOMAINS)?.map_or(Vec::new(), |d| {
+            d.split(',').map(str::to_string).collect::<Vec<String>>()
+        });
+
+        let auth_url =
+            get_env(ENV_GOOGLE_AUTH_URL)?.unwrap_or_else(|| DEFAULT_GOOGLE_AUTH_URL.to_string());
+
+        let service_account_file = get_env(ENV_GOOGLE_SERVICE_ACCOUNT)?
+            .unwrap_or_else(|| DEFAULT_GOOGLE_SERVICE_ACCOUNT.to_string())
+            .into();
+
+        let admin_email = get_env(ENV_GOOGLE_ADMIN_EMAIL)?.ok_or_else(|| {
+            error!("expected Google admin email");
+            ConfigError::MissingAdminEmail
+        })?;
+
+        GoogleConfig {
+            client_id,
+            client_secret,
+            allowed_domains,
+            auth_url,
+            service_account_file,
+            admin_email,
+        }
+    };
+
+    Ok(Config {
+        addr,
+        cookie,
+        google,
+    })
 }
 
-const fn default_port() -> u16 {
-    DEFAULT_PORT
-}
-
-fn default_cookie_name() -> String {
-    DEFAULT_COOKIE_NAME.to_string()
-}
-
-fn default_auth_url() -> String {
-    DEFAULT_AUTH_URL.to_string()
+fn get_env(key: &str) -> Result<Option<String>, ConfigError> {
+    match var(key) {
+        Ok(value) => Ok(Some(value)),
+        Err(VarError::NotPresent) => Ok(None),
+        Err(VarError::NotUnicode(_)) => {
+            error!("{} is not unicode", key);
+            Err(ConfigError::InvalidEnvVar)
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,22 @@
+//! poser is a simple, opinionated authentication provider for nginx
+//!
+//! poser authenticates with Google using OpenID Connect and then uses the
+//! Google Workspace Admin SDK to determine what groups a user is a part of.
+//! Basic information about the users, as well as what groups they are a part
+//! of, is returned to nginx in a PASETO v4 token, which is then passed to the
+//! application.
+
 mod config;
 mod error;
 mod oidc;
 mod routes;
 
 use std::env::var;
-use std::fs::read_to_string;
-use std::net::SocketAddr;
-use std::path::PathBuf;
 
 use config::Config;
 use routes::routes;
 
-use anyhow::{Context, Result};
 use axum::Server;
-use clap::Parser;
 use tower::ServiceBuilder;
 use tower_cookies::CookieManagerLayer;
 use tower_http::{
@@ -22,39 +25,18 @@ use tower_http::{
 };
 use tracing::{info, Level};
 
-#[derive(Parser, Debug)]
-#[command(author, version)]
-/// poser is a simple, opinionated authentication provider for nginx
-///
-/// poser authenticates with Google using OpenID Connect and then uses the
-/// Google Workspace Admin SDK to determine what groups a user is a part of.
-/// Basic information about the users as well as what groups they are a part
-/// of is returned to nginx in a PASETO v4 token, which is then passed to the
-/// application.
-struct Args {
-    /// Configuration file
-    #[arg(long, default_value = "/data/config/config.toml")]
-    config: PathBuf,
-}
-
 #[derive(Debug, Clone)]
 pub struct ServerState {
     pub config: Config,
 }
 
 #[tokio::main]
-async fn main() -> Result<()> {
-    init_logging();
+async fn main() {
+    tracing_subscriber::fmt()
+        .with_env_filter(var("RUST_LOG").unwrap_or_else(|_| "info".to_string()))
+        .init();
 
-    let args = Args::parse();
-
-    let config: Config = toml::from_str(&read_to_string(&args.config).with_context(|| {
-        format!(
-            "Failed to read config file at {}",
-            args.config.to_string_lossy()
-        )
-    })?)
-    .context("Failed to parse config file")?;
+    let config = Config::try_env().expect("invalid configuration");
 
     let state = ServerState {
         config: config.clone(),
@@ -73,18 +55,8 @@ async fn main() -> Result<()> {
             )
             .layer(CookieManagerLayer::new()),
     );
+    let server = Server::bind(&config.addr).serve(app.into_make_service());
 
-    let server =
-        Server::bind(&SocketAddr::from((config.ip, config.port))).serve(app.into_make_service());
-
-    info!("Serving on {}:{}", config.ip, config.port);
-    server.await.context("Server unexpectedly stopped")?;
-
-    Ok(())
-}
-
-fn init_logging() {
-    tracing_subscriber::fmt()
-        .with_env_filter(var("RUST_LOG").unwrap_or_else(|_| "info".to_string()))
-        .init();
+    info!("serving on {}", config.addr);
+    server.await.expect("server unexpectedly stopped");
 }
