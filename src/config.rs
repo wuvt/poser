e946e70b0957a9980eb8cbab3d2fe253290fc2c7
Jared Beller <jbellerb@vt.edu>
2023-03-08 01:10:24 -0500

    handle graceful shutdown

--- src/config.rs
+++ src/config.rs
@@ -3,11 +3,13 @@
 use std::env::{var, VarError};
 use std::net::SocketAddr;
 use std::path::PathBuf;
+use std::time::Duration;
 
 use base64ct::{Base64, Encoding};
 use pasetors::{keys::SymmetricKey, version4::V4};
+use regex::Regex;
 use thiserror::Error;
-use tracing::error;
+use tracing::{error, trace};
 
 /// An error that occurs while generating the server configuration.
 #[derive(Error, Clone, Debug)]
@@ -20,6 +22,8 @@ pub enum ConfigError {
     InvalidBase64,
     #[error("invalid symmetric key")]
     InvalidSymmetricKey,
+    #[error("invalid duration")]
+    InvalidDuration,
     #[error("missing cookie secret")]
     MissingCookieSecret,
     #[error("missing Google client id")]
@@ -32,6 +36,7 @@ pub enum ConfigError {
 
 // Environment variables for each config option
 pub const ENV_LISTEN_ADDR: &str = "POSER_AUTH_LISTEN_ADDR";
+pub const ENV_SHUTDOWN_GRACE_PERIOD: &str = "POSER_AUTH_SHUTDOWN_GRACE_PERIOD";
 
 pub const ENV_COOKIE_NAME: &str = "POSER_AUTH_COOKIE_NAME";
 pub const ENV_COOKIE_SECRET: &str = "POSER_AUTH_COOKIE_SECRET";
@@ -45,6 +50,7 @@ pub const ENV_GOOGLE_ADMIN_EMAIL: &str = "POSER_AUTH_GOOGLE_ADMIN_EMAIL";
 
 // Default values for some config options
 pub const DEFAULT_LISTEN_ADDR: &str = "127.0.0.1:8080";
+pub const DEFAULT_SHUTDOWN_GRACE_PERIOD: &str = "60s";
 
 pub const DEFAULT_COOKIE_NAME: &str = "_poser_auth";
 
@@ -57,6 +63,7 @@ pub struct Config {
     pub addr: SocketAddr,
     pub cookie: CookieConfig,
     pub google: GoogleConfig,
+    pub grace_period: Duration,
 }
 
 /// Settings related to handling cookies.
@@ -80,28 +87,19 @@ pub struct GoogleConfig {
 impl Config {
     /// Try to generate a new Config with environment variables.
     pub fn try_env() -> Result<Self, ConfigError> {
-        let addr = get_env_default(ENV_LISTEN_ADDR, DEFAULT_LISTEN_ADDR)?
-            .parse()
-            .map_err(|_| {
-                error!("could not parse socket address");
-                ConfigError::InvalidAddr
-            })?;
+        trace!("parsing config from environment variables");
+
+        let addr_raw = get_env_default(ENV_LISTEN_ADDR, DEFAULT_LISTEN_ADDR)?;
+        let addr = parse_socket_addr(addr_raw)?;
 
         let cookie = {
             let name = get_env_default(ENV_COOKIE_NAME, DEFAULT_COOKIE_NAME)?;
 
-            let secret_encoded = get_env(ENV_COOKIE_SECRET)?.ok_or_else(|| {
+            let secret_raw = get_env(ENV_COOKIE_SECRET)?.ok_or_else(|| {
                 error!("expected cookie secret");
                 ConfigError::MissingCookieSecret
             })?;
-            let secret_decoded = Base64::decode_vec(&secret_encoded).map_err(|_| {
-                error!("failed to decode cookie secret as base64");
-                ConfigError::InvalidBase64
-            })?;
-            let secret = SymmetricKey::<V4>::from(&secret_decoded).map_err(|_| {
-                error!("failed to interprete cookie secret as encryption key");
-                ConfigError::InvalidSymmetricKey
-            })?;
+            let secret = parse_secret_key(secret_raw)?;
 
             CookieConfig { name, secret }
         };
@@ -141,10 +139,15 @@ impl Config {
             }
         };
 
+        let grace_period_raw =
+            get_env_default(ENV_SHUTDOWN_GRACE_PERIOD, DEFAULT_SHUTDOWN_GRACE_PERIOD)?;
+        let grace_period = parse_duration(grace_period_raw)?;
+
         Ok(Config {
             addr,
             cookie,
             google,
+            grace_period,
         })
     }
 }
@@ -162,4 +165,48 @@ fn get_env(key: &str) -> Result<Option<String>, ConfigError> {
 
 fn get_env_default(key: &str, default: &str) -> Result<String, ConfigError> {
     Ok(get_env(key)?.unwrap_or_else(|| default.to_string()))
+}
+
+fn parse_socket_addr(str: String) -> Result<SocketAddr, ConfigError> {
+    str.parse().map_err(|_| {
+        error!("could not parse socket address");
+        ConfigError::InvalidAddr
+    })
+}
+
+fn parse_secret_key(str: String) -> Result<SymmetricKey<V4>, ConfigError> {
+    let decoded = Base64::decode_vec(&str).map_err(|_| {
+        error!("failed to decode cookie secret as base64");
+        ConfigError::InvalidBase64
+    })?;
+
+    SymmetricKey::<V4>::from(&decoded).map_err(|_| {
+        error!("failed to interprete cookie secret as encryption key");
+        ConfigError::InvalidSymmetricKey
+    })
+}
+
+fn parse_duration(str: String) -> Result<Duration, ConfigError> {
+    let re = Regex::new(r"^\s*(\d+)(ns|us|ms|s)?\s*$").expect("build duration regex");
+    let caps = re.captures(&str).ok_or_else(|| {
+        error!("unrecognised duration format");
+        ConfigError::InvalidDuration
+    })?;
+
+    let value = (caps[1]).parse().map_err(|_| {
+        error!("failed to parse number in duration");
+        ConfigError::InvalidDuration
+    })?;
+
+    match caps.get(2).map(|m| m.as_str()) {
+        Some("ns") => Ok(Duration::from_nanos(value)),
+        Some("us") => Ok(Duration::from_micros(value)),
+        Some("ms") => Ok(Duration::from_millis(value)),
+        Some("s") => Ok(Duration::from_secs(value)),
+        None => Ok(Duration::from_secs(value)),
+        Some(s) => {
+            error!("unrecognised duration suffix: {}", s);
+            Err(ConfigError::InvalidDuration)
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -23,46 +23,104 @@ use config::Config;
 use routes::routes;
 
 use axum::Server;
+use tokio::{
+    runtime::Runtime,
+    select,
+    signal::unix::{signal, SignalKind},
+    sync::{broadcast, mpsc},
+    time::timeout,
+};
 use tower::ServiceBuilder;
 use tower_cookies::CookieManagerLayer;
 use tower_http::{
     trace::{DefaultOnRequest, DefaultOnResponse, TraceLayer},
     LatencyUnit,
 };
-use tracing::{info, Level};
+use tracing::{debug, error, info, warn, Level};
 
 #[derive(Debug, Clone)]
 pub struct ServerState {
     pub config: Config,
+
+    // Signals back to the main thread when dropped
+    _shutdown_complete: mpsc::Sender<()>,
 }
 
-#[tokio::main]
-async fn main() {
+fn main() {
     tracing_subscriber::fmt()
         .with_env_filter(var("RUST_LOG").unwrap_or_else(|_| "info".to_string()))
         .init();
 
     let config = Config::try_env().expect("invalid configuration");
 
-    let state = ServerState {
-        config: config.clone(),
-    };
+    build_runtime().block_on(async move {
+        let (shutdown_notify, _): (broadcast::Sender<()>, broadcast::Receiver<()>) =
+            broadcast::channel(1);
+        let (shutdown_tx, shutdown_rx) = mpsc::channel(1);
 
-    let app = routes().with_state(state).layer(
-        ServiceBuilder::new()
-            .layer(
-                TraceLayer::new_for_http()
-                    .on_request(DefaultOnRequest::new().level(Level::INFO))
-                    .on_response(
-                        DefaultOnResponse::new()
-                            .level(Level::INFO)
-                            .latency_unit(LatencyUnit::Micros),
-                    ),
-            )
-            .layer(CookieManagerLayer::new()),
-    );
-    let server = Server::bind(&config.addr).serve(app.into_make_service());
+        let state = ServerState {
+            config: config.clone(),
+            _shutdown_complete: shutdown_tx.clone(),
+        };
 
-    info!("serving on {}", config.addr);
-    server.await.expect("server unexpectedly stopped");
+        let app = routes().with_state(state).layer(
+            ServiceBuilder::new()
+                .layer(
+                    TraceLayer::new_for_http()
+                        .on_request(DefaultOnRequest::new().level(Level::INFO))
+                        .on_response(
+                            DefaultOnResponse::new()
+                                .level(Level::INFO)
+                                .latency_unit(LatencyUnit::Micros),
+                        ),
+                )
+                .layer(CookieManagerLayer::new()),
+        );
+        let shutdown_signal = shutdown_notify.subscribe();
+        let server = Server::bind(&config.addr)
+            .serve(app.into_make_service())
+            .with_graceful_shutdown(wait_for_shutdown(shutdown_signal));
+
+        info!("listening on {}", config.addr);
+
+        select! {
+            _ = unix_signal(SignalKind::interrupt()) => {
+                info!("received SIGINT, shutting down");
+            }
+            _ = unix_signal(SignalKind::terminate()) => {
+                info!("received SIGTERM, shutting down");
+            }
+            Err(e) = tokio::spawn(server) => {
+                error!("server unexpectedly stopped: {}", e);
+            }
+        }
+        drop(shutdown_notify);
+        drop(shutdown_tx);
+        match timeout(config.grace_period, wait_for_complete(shutdown_rx)).await {
+            Ok(()) => debug!("shutdown completed"),
+            Err(_) => warn!(
+                "graceful shutdown did not complete in {:?}, closing anyways",
+                config.grace_period
+            ),
+        }
+    })
+}
+
+fn build_runtime() -> Runtime {
+    tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .build()
+        .expect("build threaded runtime")
+}
+
+async fn unix_signal(kind: SignalKind) {
+    signal(kind).expect("register signal handler").recv().await;
+}
+
+async fn wait_for_shutdown(mut signal: broadcast::Receiver<()>) {
+    _ = signal.recv().await;
+}
+
+async fn wait_for_complete(mut signal: mpsc::Receiver<()>) {
+    _ = signal.recv().await;
 }
