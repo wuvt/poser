82bc0e290afcc9b4160e4da0d2c249ebe2758be9
Jared Beller <jbellerb@vt.edu>
2023-03-12 22:06:10 -0400

    implement signing tests with macro

--- src/token/public.rs
+++ src/token/public.rs
@@ -80,52 +80,52 @@ mod tests {
 
     use hex_literal::hex;
 
-    const KEY_PEM: &str = "-----BEGIN PRIVATE KEY-----\nMC4CAQAwBQYDK2VwBCIEILTL+0PfTOIQcn2VPkpxMwf6Gbt9n4UEFDjZ4RuUKjd0\n-----END PRIVATE KEY-----\n";
-
     #[test]
     fn decode_pem() {
-        let key_bytes = hex!("B4CBFB43DF4CE210727D953E4A713307FA19BB7D9F85041438D9E11B942A3774");
+        let key_bytes = hex!("DF6604C6CBA2BE10CE89997C100DE0B3EF75CFAD36084651C2C983773B8C0F8F");
 
-        let key = SigningKey::from_pem(KEY_PEM).expect("decode pem");
+        let key = SigningKey::from_pem("-----BEGIN PRIVATE KEY-----\nMC4CAQAwBQYDK2VwBCIEIN9mBMbLor4QzomZfBAN4LPvdc+tNghGUcLJg3c7jA+P\n-----END PRIVATE KEY-----\n").expect("decode pem");
         assert_eq!(key.0.to_bytes(), key_bytes);
     }
 
-    #[test]
-    fn public_sign() {
-        let payload =
-            b"{\"data\":\"this is a signed message\",\"exp\":\"2022-01-01T00:00:00+00:00\"}";
-        let expected = "v4.public.eyJkYXRhIjoidGhpcyBpcyBhIHNpZ25lZCBtZXNzYWdlIiwiZXhwIjoiMjAyMi0wMS0wMVQwMDowMDowMCswMDowMCJ9bg_XBBzds8lTZShVlwwKSgeKpLT3yukTw6JUz3W4h_ExsQV-P0V54zemZDcAxFaSeef1QlXEFtkqxT1ciiQEDA";
+    macro_rules! test_vector {
+        ($vec_name:ident, $token:expr, $payload:expr, $footer:expr, $implicit:expr) => {
+            #[test]
+            fn $vec_name() {
+                let token: &str = $token;
+                let payload: &[u8] = $payload;
+                let footer: Option<&[u8]> = $footer;
+                let implicit: Option<&[u8]> = $implicit;
 
-        let key = SigningKey::from_pem(KEY_PEM).expect("decode pem");
+                let key = SigningKey::from_pem("-----BEGIN PRIVATE KEY-----\nMC4CAQAwBQYDK2VwBCIEILTL+0PfTOIQcn2VPkpxMwf6Gbt9n4UEFDjZ4RuUKjd0\n-----END PRIVATE KEY-----\n").expect("decode pem");
 
-        let token = key.sign_message(payload, None, None);
-        assert_eq!(token, expected);
+                let sign = key.sign_message(payload, footer, implicit);
+                assert_eq!(sign, token);
+            }
+        };
     }
 
-    #[test]
-    fn public_sign_with_footer() {
-        let payload =
-            b"{\"data\":\"this is a signed message\",\"exp\":\"2022-01-01T00:00:00+00:00\"}";
-        let footer = b"{\"kid\":\"zVhMiPBP9fRf2snEcT7gFTioeA9COcNy9DfgL1W60haN\"}";
-        let expected = "v4.public.eyJkYXRhIjoidGhpcyBpcyBhIHNpZ25lZCBtZXNzYWdlIiwiZXhwIjoiMjAyMi0wMS0wMVQwMDowMDowMCswMDowMCJ9v3Jt8mx_TdM2ceTGoqwrh4yDFn0XsHvvV_D0DtwQxVrJEBMl0F2caAdgnpKlt4p7xBnx1HcO-SPo8FPp214HDw.eyJraWQiOiJ6VmhNaVBCUDlmUmYyc25FY1Q3Z0ZUaW9lQTlDT2NOeTlEZmdMMVc2MGhhTiJ9";
+    test_vector!(
+        vec_4_s_1,
+        "v4.public.eyJkYXRhIjoidGhpcyBpcyBhIHNpZ25lZCBtZXNzYWdlIiwiZXhwIjoiMjAyMi0wMS0wMVQwMDowMDowMCswMDowMCJ9bg_XBBzds8lTZShVlwwKSgeKpLT3yukTw6JUz3W4h_ExsQV-P0V54zemZDcAxFaSeef1QlXEFtkqxT1ciiQEDA",
+        b"{\"data\":\"this is a signed message\",\"exp\":\"2022-01-01T00:00:00+00:00\"}",
+        None,
+        None
+    );
 
-        let key = SigningKey::from_pem(KEY_PEM).expect("decode pem");
+    test_vector!(
+        vec_4_s_2,
+        "v4.public.eyJkYXRhIjoidGhpcyBpcyBhIHNpZ25lZCBtZXNzYWdlIiwiZXhwIjoiMjAyMi0wMS0wMVQwMDowMDowMCswMDowMCJ9v3Jt8mx_TdM2ceTGoqwrh4yDFn0XsHvvV_D0DtwQxVrJEBMl0F2caAdgnpKlt4p7xBnx1HcO-SPo8FPp214HDw.eyJraWQiOiJ6VmhNaVBCUDlmUmYyc25FY1Q3Z0ZUaW9lQTlDT2NOeTlEZmdMMVc2MGhhTiJ9",
+        b"{\"data\":\"this is a signed message\",\"exp\":\"2022-01-01T00:00:00+00:00\"}",
+        Some(b"{\"kid\":\"zVhMiPBP9fRf2snEcT7gFTioeA9COcNy9DfgL1W60haN\"}"),
+        None
+    );
 
-        let token = key.sign_message(payload, Some(footer), None);
-        assert_eq!(token, expected);
-    }
-
-    #[test]
-    fn public_sign_with_implicit() {
-        let payload =
-            b"{\"data\":\"this is a signed message\",\"exp\":\"2022-01-01T00:00:00+00:00\"}";
-        let footer = b"{\"kid\":\"zVhMiPBP9fRf2snEcT7gFTioeA9COcNy9DfgL1W60haN\"}";
-        let implicit = b"{\"test-vector\":\"4-S-3\"}";
-        let expected = "v4.public.eyJkYXRhIjoidGhpcyBpcyBhIHNpZ25lZCBtZXNzYWdlIiwiZXhwIjoiMjAyMi0wMS0wMVQwMDowMDowMCswMDowMCJ9NPWciuD3d0o5eXJXG5pJy-DiVEoyPYWs1YSTwWHNJq6DZD3je5gf-0M4JR9ipdUSJbIovzmBECeaWmaqcaP0DQ.eyJraWQiOiJ6VmhNaVBCUDlmUmYyc25FY1Q3Z0ZUaW9lQTlDT2NOeTlEZmdMMVc2MGhhTiJ9";
-
-        let key = SigningKey::from_pem(KEY_PEM).expect("decode pem");
-
-        let token = key.sign_message(payload, Some(footer), Some(implicit));
-        assert_eq!(token, expected);
-    }
+    test_vector!(
+        vec_4_s_3,
+        "v4.public.eyJkYXRhIjoidGhpcyBpcyBhIHNpZ25lZCBtZXNzYWdlIiwiZXhwIjoiMjAyMi0wMS0wMVQwMDowMDowMCswMDowMCJ9NPWciuD3d0o5eXJXG5pJy-DiVEoyPYWs1YSTwWHNJq6DZD3je5gf-0M4JR9ipdUSJbIovzmBECeaWmaqcaP0DQ.eyJraWQiOiJ6VmhNaVBCUDlmUmYyc25FY1Q3Z0ZUaW9lQTlDT2NOeTlEZmdMMVc2MGhhTiJ9",
+        b"{\"data\":\"this is a signed message\",\"exp\":\"2022-01-01T00:00:00+00:00\"}",
+        Some(b"{\"kid\":\"zVhMiPBP9fRf2snEcT7gFTioeA9COcNy9DfgL1W60haN\"}"),
+        Some(b"{\"test-vector\":\"4-S-3\"}")
+    );
 }
