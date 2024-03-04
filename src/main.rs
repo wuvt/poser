44145b39ce07ed0463ca7b561c09effd3599bef2
jae beller <foss@jae.zone>
2024-03-03 21:25:33 -0500

    clean up graceful shutdown

--- src/main.rs
+++ src/main.rs
@@ -1,69 +1,39 @@
-//! # poser
-//!
-//! poser is a simple, opinionated authentication provider for nginx
-//!
-//! ## About
-//!
-//! poser authenticates with Google using OpenID Connect and then uses the
-//! Google Workspace Admin SDK to determine what groups a user is a part of.
-//! Basic information about the user and what groups they are a part of is
-//! returned to nginx in a [Paseto v4] token, which is then passed to the
-//! application.
-//!
-//! [Paseto v4]: https://github.com/paseto-standard/paseto-spec
-
-pub mod config;
-pub mod error;
-pub mod oidc;
-pub mod routes;
-pub mod token;
-
 use std::env::var;
 use std::future::IntoFuture;
 use std::sync::Arc;
 
-use config::Config;
-use oidc::setup_auth;
-use openidconnect::core::CoreClient;
-use routes::routes;
+use poser::config::Config;
+use poser::oidc::setup_auth;
+use poser::shutdown;
+use poser::{routes, ServerState};
 
+use anyhow::Context;
 use tokio::{
     net::TcpListener,
     runtime::Runtime,
-    select,
     signal::unix::{signal, SignalKind},
-    sync::{broadcast, mpsc},
     time::timeout,
 };
-use tokio_postgres::{Client, NoTls};
+use tokio_postgres::NoTls;
 use tower::ServiceBuilder;
 use tower_cookies::CookieManagerLayer;
-use tower_http::{
-    trace::{DefaultOnRequest, DefaultOnResponse, TraceLayer},
-    LatencyUnit,
-};
+use tower_http::trace::{DefaultOnRequest, DefaultOnResponse, TraceLayer};
 use tracing::{debug, error, info, warn, Level};
-
-#[derive(Debug, Clone)]
-pub struct ServerState {
-    pub config: Config,
-    pub db: Arc<Client>,
-    pub oidc: CoreClient,
-
-    // Signals back to the main thread when dropped
-    _shutdown_complete: mpsc::Sender<()>,
-}
 
 fn main() {
     tracing_subscriber::fmt()
-        .with_env_filter(var("RUST_LOG").unwrap_or_else(|_| "info".to_string()))
+        .with_env_filter(var("RUST_LOG").unwrap_or_else(|_| "warn,poser=info".to_string()))
         .init();
 
-    let config = Config::try_env().expect("invalid configuration");
+    let config = Config::try_env()
+        .context("failed to build config")
+        .unwrap_or_else(|e| {
+            error!("{:#}", e);
+            std::process::exit(1);
+        });
 
     build_runtime().block_on(async move {
-        let (shutdown_notify, _) = broadcast::channel(1);
-        let (shutdown_tx, shutdown_rx) = mpsc::channel(1);
+        let shutdown = shutdown::Sender::new();
 
         let (db, conn) = tokio_postgres::connect(&config.database, NoTls)
             .await
@@ -77,15 +47,15 @@ fn main() {
             config: config.clone(),
             db: Arc::new(db),
             oidc,
-            _shutdown_complete: shutdown_tx.clone(),
+            shutdown: shutdown.subscribe(),
         };
 
-        let db_signal = shutdown_tx.clone();
-        let conn = tokio::spawn(async move {
+        let postgres_notify = shutdown.subscribe();
+        let postgres = async move {
             let res = conn.await;
-            drop(db_signal);
+            drop(postgres_notify);
             res
-        });
+        };
 
         let router = routes().with_state(state).layer(
             ServiceBuilder::new()
@@ -95,28 +65,28 @@ fn main() {
                         .on_response(
                             DefaultOnResponse::new()
                                 .level(Level::INFO)
-                                .latency_unit(LatencyUnit::Micros),
+                                .latency_unit(tower_http::LatencyUnit::Micros),
                         ),
                 )
                 .layer(CookieManagerLayer::new()),
         );
+
         let listener = TcpListener::bind(&config.addr)
             .await
-            .expect("failed to bind to socket");
-        let shutdown_signal = shutdown_notify.subscribe();
+            .context("failed to bind to socket")
+            .unwrap_or_else(|e| {
+                error!("{:#}", e);
+                std::process::exit(1);
+            });
+
+        let mut axum_notify = shutdown.subscribe();
         let server = axum::serve(listener, router)
-            .with_graceful_shutdown(wait_for_shutdown(shutdown_signal));
+            .with_graceful_shutdown(async move { _ = axum_notify.recv().await });
 
-        info!("listening on {}", config.addr);
-
-        select! {
-            _ = unix_signal(SignalKind::interrupt()) => {
-                info!("received SIGINT, shutting down");
-            },
-            _ = unix_signal(SignalKind::terminate()) => {
-                info!("received SIGTERM, shutting down");
-            },
-            res = conn => match res {
+        info!("listening for connections on: {}", config.addr);
+        tokio::select! {
+            sig = shutdown_signal() => info!("received {}, starting graceful shutdown...", sig),
+            res = tokio::spawn(postgres) => match res {
                 Ok(Ok(_)) => error!("database connection closed unexpectedly"),
                 Ok(Err(e)) => error!("database connection error: {}", e),
                 Err(e) => error!("database executor unexpectedly stopped: {}", e),
@@ -127,14 +97,16 @@ fn main() {
                 Err(e) => error!("server executor unexpectedly stopped: {}", e),
             },
         }
-        drop(shutdown_notify);
-        drop(shutdown_tx);
-        match timeout(config.grace_period, wait_for_complete(shutdown_rx)).await {
-            Ok(()) => debug!("shutdown completed"),
-            Err(_) => warn!(
-                "graceful shutdown did not complete in {:?}, closing anyways",
-                config.grace_period
-            ),
+
+        tokio::select! {
+            sig = shutdown_signal() => error!("received second {}, aborting.", sig),
+            res = timeout(config.grace_period, shutdown::Sender::shutdown(shutdown)) => match res {
+                Ok(()) => debug!("shutdown completed"),
+                Err(_) => warn!(
+                    "graceful shutdown did not complete in {:?}, closing anyways",
+                    config.grace_period
+                ),
+            },
         }
     })
 }
@@ -143,17 +115,27 @@ fn build_runtime() -> Runtime {
     tokio::runtime::Builder::new_multi_thread()
         .enable_all()
         .build()
-        .expect("build threaded runtime")
+        .context("failed to build threaded runtime")
+        .unwrap_or_else(|e| {
+            error!("{:#}", e);
+            std::process::exit(1);
+        })
 }
 
-async fn unix_signal(kind: SignalKind) {
-    signal(kind).expect("register signal handler").recv().await;
-}
+async fn shutdown_signal() -> &'static str {
+    async fn wait_for_signal(kind: SignalKind) {
+        signal(kind)
+            .context("failed to register signal handler")
+            .unwrap_or_else(|e| {
+                error!("{:#}", e);
+                std::process::exit(1);
+            })
+            .recv()
+            .await;
+    }
 
-async fn wait_for_shutdown(mut signal: broadcast::Receiver<()>) {
-    _ = signal.recv().await;
-}
-
-async fn wait_for_complete(mut signal: mpsc::Receiver<()>) {
-    _ = signal.recv().await;
+    tokio::select! {
+        _ = wait_for_signal(SignalKind::interrupt()) => "SIGINT",
+        _ = wait_for_signal(SignalKind::terminate()) => "SIGTERM",
+    }
 }
